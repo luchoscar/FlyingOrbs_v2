ff9c91be32a27916bf7ed328da5cf1952d8a5630
luchoscar <losh177@hotmail.com>
2017-05-30 00:43:53 -0700

    Storing resources in list

--- Assets/3D-World/Scripts/PlanetData.cs
+++ Assets/3D-World/Scripts/PlanetData.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetData : MonoBehaviour
@@ -8,17 +9,23 @@ public class PlanetData : MonoBehaviour
     public float resourcesForwardSpeed;
     public float resourcesTurnSpeed;
     public float collisionAngle {  get { return m_collisionAngle; } }
+    public List<ResourceOrbit> resources {  get { return _resources; } }
 
     public GameObject resourceObject;
 
     public void Init()
     {
+        _resources = new List<ResourceOrbit>(resourcesCount);
+
         for (int i = 0; i < resourcesCount; i++)
         {
             Vector3 direction = Random.onUnitSphere;
             GameObject resourceGO = GameObject.Instantiate(resourceObject) as GameObject;
 
-            resourceGO.GetComponent<ResourceOrbit>().LandOnPlanet(this);
+            ResourceOrbit resourceOrbit = resourceGO.GetComponent<ResourceOrbit>();
+            resourceOrbit.LandOnPlanet(this);
+
+            _resources.Add(resourceOrbit);
         }
 
         m_collisionAngle = _calculateResourceCollisionAngle();
@@ -35,4 +42,6 @@ public class PlanetData : MonoBehaviour
 
     [SerializeField]
     private float m_collisionAngle;
+
+    private List<ResourceOrbit> _resources = null;
 }
